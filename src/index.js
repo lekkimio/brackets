cce0e2dac84d15d6f1db211c815ced5ab8c7567d
lekkimio <70840990+lekkimio@users.noreply.github.com>
2020-10-25 17:18:32 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map((config) => config.join(''));
+    for (let i = 0; i < bracketsConfig.length;) {
+        if (str.includes(bracketsConfig[i])) {
+            console.log(bracketsConfig[i]);
+            str = str.replace(bracketsConfig[i], '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    return str ? false : true;
 }
